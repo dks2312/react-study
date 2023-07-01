d3c19a077e8fd0e1ebad09d501b65a3135d8759d
broanexdev2018! <59419591+dks2312@users.noreply.github.com>
2023-07-01 19:23:27 +0900

    Update Counter.js

--- velopert/p01-react-basic/begin-react/src/Counter.js
+++ velopert/p01-react-basic/begin-react/src/Counter.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Counter() {
+
+    const [number, setNumber] = useState(0);
+
+    const onIncrease = () => {
+      setNumber(number + 1);
+    }
+  
+    const onDecrease = () => {
+      setNumber(number - 1);
+    }
+    
+    const onCh = (e) => {
+        setText(e.target.value);
+    }
+
+    const onCi = () => {
+        setText('');
+    }
+    
   return (
     <div>
-      <h1>0</h1>
-      <button>+1</button>
-      <button>-1</button>
+      <h1>{number}</h1>
+      <button onClick={onIncrease}>+1</button>
+      <button onClick={onDecrease}>-1</button>
+      <input placeholder="이름" onChange={onCh} />
+      <button onClick={onCi}>초기화</button>
     </div>
   );
 }
